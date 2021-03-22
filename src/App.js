913ff7f39b5dac37d68ea781aa07ae7fa19b759b
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2021-03-22 11:17:19 +0400

    initial state

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import style from './App.module.css';
+
+const App = (props) => {
+    return ( <
+        div className = { style.container } >
+
+        <
+        h1 > Movie < /h1>
+
+        <
+        /div>
+    );
+}
+
+export default App;
