e2f295f05a8b281baa0f6b284baab68c9809879c
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2021-05-12 12:30:13 +0400

    replace icon filter

--- src/components/navbarContainer/navbar/filter/Filter.jsx
+++ src/components/navbarContainer/navbar/filter/Filter.jsx
@@ -4,17 +4,30 @@ import FilterForm from './filterForm/FilterForm';
 
 const Filter = ({watchMovieByYear, page}) => {
     const [filterDialog, openFilterDialog] = useState(false);
+    const [arrowIcon, setArrowIcon] = useState('fas fa-angle-right');
 
     const getFilterByYearValue = (value) => {
+        //get new movie by year
         watchMovieByYear(page, value.dateFrom, value.dateTo);
+        //close filter dialog
         openFilterDialog(!filterDialog);
+        //replace icon filter
+        setArrowIcon('fas fa-angle-right');
     }
-  
+    
+    const clickOnFilter = () => {
+        //open filter dialog
+        openFilterDialog(!filterDialog);
+        //replace icon filter
+        const replaceIcon = filterDialog? 'fas fa-angle-right' : 'fas fa-angle-down'
+        setArrowIcon(replaceIcon);
+    }
+
     return (
         <div>
-            <li onClick={()=>openFilterDialog(!filterDialog)} className={style.menuItem}>
+            <li onClick={clickOnFilter} className={style.menuItem}>
                 <span>Filter</span>
-                <i className='fas fa-angle-right'></i>
+                <i className={arrowIcon}></i>
             </li>
             
             {filterDialog?
