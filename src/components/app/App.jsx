3c7cf1f1a7b9918336b638ad8a43de9cb9378de6
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2021-05-15 22:51:16 +0400

    make corrections to the App

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import style from './App.module.css';
 import { Route } from 'react-router-dom';
+import style from './App.module.css';
 import PopularContainer from '../popularContainer/PopularContainer.jsx';
-import NavbarContainer from '../navbarContainer/NavbarContainer';
-import Footer from '../footer/Footer';
-import HeaderContainer from '../headerContainer/HeaderContainer';
+import NavbarContainer from '../navbarContainer/NavbarContainer.jsx';
+import Footer from '../footer/Footer.jsx';
+import HeaderContainer from '../headerContainer/HeaderContainer.jsx';
 
 const App = () => {
 
-    return (
+  return (
 
-        <div className = {style.container}>
-          <HeaderContainer/>
-          <menu>
-            <NavbarContainer />
-            <Route path = '/'> <PopularContainer/> </Route> 
-          </menu> 
-          <Footer/>
-        </div>
-    )
+    <div className={style.container}>
+      <HeaderContainer />
+      <menu>
+        <NavbarContainer />
+        <Route path='/'> <PopularContainer /> </Route>
+      </menu>
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
